8e184107d6eee640362cdfb7036c5297b58bad58
milohill <116620023+grimhilt7@users.noreply.github.com>
2023-05-05 16:22:46 +0900

    Fix the app file again

--- controllers/kind-controller.js
+++ controllers/kind-controller.js
@@ -1,20 +1,8 @@
 const mongoose = require('mongoose');
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
-const multer = require('multer');
-const fs = require('file-system');
 const Kind = require('../models/kind');
 const Backpack = require('../models/backpack');
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads');
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${file.fieldname}-${Date.now()}`);
-  },
-});
-const upload = multer({ storage: storage });
 
 exports.list_kinds = asyncHandler(async (req, res, next) => {
   try {
